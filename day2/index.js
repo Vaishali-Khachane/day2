fb48ae132c1bb780e0ea7efaaebd39e513450f26
Vaishali-Khachane <81966589+Vaishali-Khachane@users.noreply.github.com>
2021-09-11 15:03:28 +0530

    Add files via upload

---
+++ day2/index.js
@@ -0,0 +1,42 @@
+
+var a = 6; 
+
+var b = 1; 
+
+  
+// AND Operation 
+
+document.write("A & B = " + (a & b) + '<br>'); 
+
+  
+
+  
+// OR operation 
+
+document.write("A | B = " + (a | b) + '<br>'); 
+
+  
+// NOT operation 
+
+document.write("~A = " + (~a) + '<br>'); 
+
+  
+
+  
+// Sign Propagating Right Shift 
+
+document.write("A >> B = " + (a >> b) + '<br>'); 
+
+  
+
+  
+// Zero Fill Right Shift 
+
+document.write("A >>> B = " + (a >>> b) + '<br>'); 
+
+  
+
+  
+// Left Shift 
+
+document.write("A << B = " + (a << b) + '<br>');  
